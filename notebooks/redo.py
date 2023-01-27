5fa605fbd5435b7dac2ccfe081317db8fe5e4d89
ShixingWang <wangshixing@wustl.edu>
2023-01-26 23:26:47 -0600

    some thoughts before redo.

---
+++ notebooks/redo.py
@@ -0,0 +1,4 @@
+# Q1: Does z-stack really move the central plane of imaging?
+#     Q1.1: What is the relationship between PFSed single z and z stacks?
+#     Q1.2: Should we image color by color, or detector by detector?
+# Q2: What is the correlation between 2044*2048 and 1024*1024, is it really central cropping?
