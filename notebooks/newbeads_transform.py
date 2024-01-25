cd8f99df3eaeb694b9e2d69f357fe03d43bb12a7
ShixingWang <wangshixing@wustl.edu>
2024-01-25 16:46:20 -0600

    rename the output files

--- notebooks/newbeads_transform.py
+++ notebooks/newbeads_transform.py
@@ -112,5 +112,5 @@ for fov,by in ((2,"detector"),(2,"color"),(3,"detector")):
         transf = transform.AffineTransform()
         success = transf.estimate(xy_src,xy_dst)
         if success:
-            np.savetxt(f"intermediate/transforms/new-beads_{c_src}-512_{c_dst}-1024.txt",transf.params)
+            np.savetxt(f"intermediate/transforms/new-beads_{c_src}-1024_{c_dst}-512.txt",transf.params)
             print("...saved once.")
